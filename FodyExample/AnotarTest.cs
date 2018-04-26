4c7ab5248617a892d0427ae524b2fa8192bd9bd8
Dmitry Novik <dmitry.novik@gmail.com>
2018-04-26 15:27:37 +1000

    Switching from console app to the library with tests

---
+++ FodyExample/AnotarTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Anotar.NLog;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using Xunit;
+
+namespace FodyExample
+{
+    public class AnotarTest
+    {
+        [Fact]
+        public void Anotar()
+        {
+            var config = new LoggingConfiguration();
+            var consoleTarget = new ColoredConsoleTarget();
+            config.AddTarget("console", consoleTarget);
+            var rule = new LoggingRule("*", LogLevel.Debug, consoleTarget);
+            config.LoggingRules.Add(rule);
+            LogManager.Configuration = config;
+
+            Console.WriteLine("\nTesting Anotar");
+            LogTo.Info("Dmitry");
+        }
+    }
+}
---
+++ FodyExample/MethodTimerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MethodTimer;
+using Xunit;
+
+namespace FodyExample
+{    
+    public class MethodTimerTest
+    {
+        [Fact]
+        public void MethodTimer()
+        {
+            Console.WriteLine("Testing Fody.MethodTimer");
+            RunTimedTask();
+        }
+
+        [Time]
+        private static void RunTimedTask(int until = int.MaxValue >> 2)
+        {
+            for (int i = 0; i < until; ++i) { }
+        }
+    }
+}
