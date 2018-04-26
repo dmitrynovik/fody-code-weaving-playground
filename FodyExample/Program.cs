e3d76c30ace72824ac566c2d275a8823717663f9
Dmitry Novik <dmitry.novik@gmail.com>
2018-04-26 15:01:18 +1000

    anotar samples

--- FodyExample/Program.cs
+++ FodyExample/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Anotar.NLog;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace FodyExample
 {
@@ -6,12 +10,35 @@ namespace FodyExample
     {
         static int Main()
         {
+            MethodTimer();
+            Anotar();
+            Done();
+            return 0;
+        }
+
+        private static void MethodTimer()
+        {
             Console.WriteLine("Testing Fody.MethodTimer");
             Timed.RunTimedTask();
+        }
 
+        private static void Anotar()
+        {
+            var config = new LoggingConfiguration();
+            var consoleTarget = new ColoredConsoleTarget();
+            config.AddTarget("console", consoleTarget);
+            var rule = new LoggingRule("*", LogLevel.Debug, consoleTarget);
+            config.LoggingRules.Add(rule);
+            LogManager.Configuration = config;
+
+            Console.WriteLine("\nTesting Anotar");
+            LogTo.Info("Dmitry");
+        }
+
+        private static void Done()
+        {
             Console.WriteLine("Press any key to exit");
             Console.Read();
-            return 0;
         }
     }
 }
