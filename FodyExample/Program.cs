4039a48313f1948c7b0ef860875faf722ff86441
Dmitry Novik <dmitry.novik@gmail.com>
2018-04-26 14:05:39 +1000

    Add project files.

---
+++ FodyExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FodyExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
