f28752379f0390489e7adfa9ebf7876420a70d00
Dmitry Novik <dmitry.novik@gmail.com>
2018-04-26 14:38:35 +1000

    added MethodTimeLogger

---
+++ FodyExample/MethodTimeLogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+
+namespace FodyExample
+{
+    public static class MethodTimeLogger
+    {
+        public static void Log(MethodBase methodBase, long milliseconds) =>
+            Console.WriteLine($"{methodBase} took {milliseconds}ms.");
+    }
+}
--- FodyExample/Program.cs
+++ FodyExample/Program.cs
@@ -1,17 +1,22 @@
-﻿using MethodTimer;
+﻿using System;
+using MethodTimer;
 
 namespace FodyExample
 {
     class Program
     {
-        static int Main(string[] args)
+        static int Main()
         {
+            Console.WriteLine("Testing Fody.MethodTimer");
             RunTimedTask();
+
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
             return 0;
         }
 
         [Time]
-        private static void RunTimedTask(int until = int.MaxValue)
+        private static void RunTimedTask(int until = int.MaxValue >> 2)
         {
             for (int i = 0; i < until; ++i) { }
         }
